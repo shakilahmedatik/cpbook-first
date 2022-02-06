20a81482426180aa7651776c9e2dd892c65c2a1b
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-02-06 16:38:34 +0600

    chapter-2 p6 done

---
+++ chapter-2/p6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, sum;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    sum = a + b;
+
+    printf("Sum of %d + %d is %d", a, b, sum);
+
+    return 0;
+}
