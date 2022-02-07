c8a7efa57ec79498202e20d180e4c0fd2856e4bc
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-02-07 15:19:34 +0600

    chapter-3 p1 done

---
+++ chapter-3/p1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n >= 0)
+    {
+        printf("%d is a positive number", n);
+    }
+    else
+    {
+        printf("%d is a negative number", n);
+    }
+    return 0;
+}
