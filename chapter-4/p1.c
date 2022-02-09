d6902b4dec12ed98c6e4c6b2a91e38ec33a768b1
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-02-09 23:47:17 +0600

    chapter-4 p1 done

--- chapter-4/p1.c
+++ chapter-4/p1.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("Chapter 4: Loop & more!");
+    int n = 1, limit;
+    printf("Enter a number: ");
+    scanf("%d", &limit);
+    while (n <= limit)
+    {
+        printf("%d\n", n);
+        n++;
+    }
     return 0;
 }
