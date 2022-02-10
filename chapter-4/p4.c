c73007a526e6885547ab80ad6a62a73d8b106c58
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-02-10 22:38:25 +0600

    chapter-4 p4 done

--- chapter-4/p4.c
+++ chapter-4/p4.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
+// Multiplication of 5
+
 int main()
 {
-    int n = 1, limit;
-    printf("Enter a number: ");
-    scanf("%d", &limit);
-    while (n <= 100)
+    int n, i = 1;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    while (i < 11)
     {
-        printf("%d\n", n);
-        n++;
+        printf("%d * %d = %d\n", n, i, n * i);
+        i++;
     }
     return 0;
 }
