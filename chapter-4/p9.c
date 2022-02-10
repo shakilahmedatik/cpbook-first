84aaa46b9588db0543c3e9f980722951c4a3bf96
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-02-10 23:41:23 +0600

    chapter-4 p9 done

---
+++ chapter-4/p9.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// 1,2,3 numbers all possible permutations from lowest to highest orders
+
+int main()
+{
+    int a, b, c;
+
+    for (a = 1; a <= 3; a++)
+    {
+        for (b = 1; b <= 3; b++)
+        {
+            if (b != a)
+            {
+                for (c = 1; c <= 3; c++)
+                {
+                    if (c != a && c != b)
+                    {
+                        printf("%d, %d, %d\n", a, b, c);
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
