323c751146e50c01c0240c75a758fa6e45a6daa6
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-16 20:30:04 +0600

    chapter-6 p3 done

---
+++ chapter-6/p3.c
@@ -0,0 +1,12 @@
+// Array in C language
+#include <stdio.h>
+
+int main()
+{
+    int arr[5] = {
+        10, 20, 30, 40, 50};
+
+    printf("First element is: %d\n", arr[0]);
+    printf("Third element is: %d", arr[2]);
+    return 0;
+}
