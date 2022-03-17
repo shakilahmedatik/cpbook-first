3dfabda480dd8ee40245d773b740accf225b8d63
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-17 13:14:03 +0600

    Reverse all the element of an array using temp in c

--- chapter-6/p8.c
+++ chapter-6/p8.c
@@ -1,11 +1,12 @@
 // Reverse all the element of an array using temp in c
 #include <stdio.h>
+#define n 10
 int main()
 {
-    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int arr[n] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 95};
 
     int i, j, temp;
-    for (i = 0, j = 9; i < 10; i++, j--)
+    for (i = 0, j = 9; i < n / 2; i++, j--)
     {
         temp = arr[j];
         arr[j] = arr[i];
