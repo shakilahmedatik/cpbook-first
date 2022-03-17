3e36fd3897c61028f44e92199706c66d0205bfff
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-17 12:52:55 +0600

    Reverse all the element of an array in c

---
+++ chapter-6/p7.c
@@ -0,0 +1,22 @@
+// Reverse all the element of an array in c
+#include <stdio.h>
+int main()
+{
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int arr2[10];
+    int i, j;
+    for (i = 0, j = 9; i < 10; i++, j--)
+    {
+        arr2[j] = arr[i];
+    }
+    for (i = 0; i < 10; i++)
+    {
+        arr[i] = arr2[i];
+    }
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
