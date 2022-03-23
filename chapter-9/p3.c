7e3f37e6975528db20a675e9f6198175b19f9ee3
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-23 17:38:48 +0600

    chapter-9 p3 done

---
+++ chapter-9/p3.c
@@ -0,0 +1,10 @@
+// string representation ic C Programming Language
+#include <stdio.h>
+
+int main()
+{
+    char country[] = {'B', 'A', 'N', 'G', 'L', 'A', 'D', 'E', 'S', 'H', '\0', 'I', 'S', '\0', 'M', 'Y', '\0', 'C', 'O', 'U', 'N', 'T', 'R', 'Y', '\0'};
+
+    printf("%s\n", country);
+    return 0;
+}
