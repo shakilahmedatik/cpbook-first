6f403f998d39f1cebb3246a3d4cfce2d991a3d0c
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-23 15:36:56 +0600

    chapter-9 p1 done

---
+++ chapter-9/p1.c
@@ -0,0 +1,11 @@
+// String Representation in C Programming Language.
+#include <stdio.h>
+int main()
+{
+    char country[11] = {'B', 'A', 'N', 'G', 'L', 'A', 'D', 'E', 'S', 'H', '\0'};
+    char country[] = {'B', 'A', 'N', 'G', 'L', 'A', 'D', 'E', 'S', 'H', '\0'};
+    char country[] = "Bangladesh";
+    char *country = "Bangladesh";
+
+    return 0;
+}
