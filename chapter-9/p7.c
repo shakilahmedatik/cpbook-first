b94b5925c95cdbfb90d53e31d5032753df994a16
Shakil Ahmed Atik <shakilatik@gmail.com>
2022-03-23 18:30:19 +0600

    chapter-9 p7 done

---
+++ chapter-9/p7.c
@@ -0,0 +1,24 @@
+// String length count using C Programming Language
+#include <stdio.h>
+int string_length(char str[])
+{
+    int i, length = 0;
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        length++;
+    }
+    return length;
+}
+
+int main()
+{
+    char country[100];
+    int length;
+
+    while (1 == scanf("%s", country))
+    {
+        length = string_length(country);
+        printf("%d\n", length);
+    }
+    return 0;
+}
